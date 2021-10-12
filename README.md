147e0d5c7bc852cb799dbdaec26a64bdd19ba436
Lauric Desauw <lauric.desauw@inria.fr>
2021-10-12 11:10:02 +0200

    feat: refactor the repositoty

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# Procfs-sensor
+VirtualWatts is a software-defined power meter based on the PowerAPI toolkit.
+VirtualWatts a configurable software that can estimate the power consumption of
+process inside a VM in real-time.
+VirtuallWatts is supposed to be used inside a VM. It need to receive two metrics :
 
-Proc file system Sensor is a tool that monitor the CPU usage of process using the proc
-file system.
-
-The sensor use the proc file system of the Linux kernel. It is only available on Linux.
-
-The sensor could be used in a virtual machine.
+- The energy consumption of the VM, usually made with
+  [SmartWatts](https://github.com/powerapi-ng/smartwatts-formula)
+- The cpu usage of the tracked processes and the global cpu usage, as provided
+  by [procfs-sensor](https://github.com/powerapi-ng/procfs-sensor).
 
 # About
 
-Procfs sensor is an open-source project developed by the [Spirals research
+VirtualWatts is an open-source project developed by the [Spirals research
 group](https://team.inria.fr/spirals) (University of Lille 1 and Inria).
 
 The documentation is not available yet.
+
+## Contributing
+
+If you would like to contribute code you can do so through GitHub by forking the
+repository and sending a pull request.
+You should start by reading the [contribution guide](https://github.com/powerapi-ng/virtualwatts/blob/main/contributing.md)
+
+When submitting code, please check that it is conform by using `pylint` and
+`flake8` with the configurations files at the root of the project.
+
+## Acknowledgments
+
+VirtualWatts is written in [Python](https://www.python.org/) (under [PSF
+license](https://docs.python.org/3/license.html)) and built on top of
+[PowerAPI](https://github.com/powerapi-ng/powerapi)
--- procfs_sensor/__main__.py
+++ procfs_sensor/__main__.py
@@ -34,6 +34,7 @@ import sys
 import socket
 import threading
 
+
 def read_config(config_file):
     """ Read the config from the config_files specified in argument"""
     file_object = open(config_file, "r")
@@ -42,19 +43,18 @@ def read_config(config_file):
     return json.loads(json_content)
 
 
-
 def mesure_cpu_usage():
     """Mesure the cpu usage of process using pidstat"""
-    timestamp=datetime.today()
+    timestamp = datetime.today()
     raw_stat = str(subprocess.check_output(["pidstat"]))
 
     stat = raw_stat.split('\\n')
     pid_cpu_usage = {}
-    for i in range(3,len(stat)) :
+    for i in range(3, len(stat)):
         pid_stat = stat[i].split(' ')
 
         pid_stat = list(filter(('').__ne__, pid_stat))
-        if len(pid_stat) != 10 :
+        if len(pid_stat) != 10:
             continue
 
         pid_cpu_usage[pid_stat[2]] = pid_stat[7]
@@ -62,23 +62,22 @@ def mesure_cpu_usage():
     return timestamp, pid_cpu_usage
 
 
-def send_tcp_report(sock ,report):
+def send_tcp_report(sock, report):
     """ Send the json report using TCP"""
-
 
     to_send = report.encode('utf-8')
     sock.sendall(to_send)
 
 
-def send_report(sock,report):
+def send_report(sock, report):
     """ Send the json report using the output method specified in the config"""
 
-    send_tcp_report(sock,report)
+    send_tcp_report(sock, report)
 
 
-def sensor_mesure_send(frequency,sensor,target,sock):
+def sensor_mesure_send(sampling_interval, sensor, target, sock):
     """ Produce the report from scratch and send it"""
-    probe = threading.Timer(frequency/1000,sensor_mesure_send,[frequency,sensor,target,sock])
+    probe = threading.Timer(sampling_interval / 1000, sensor_mesure_send, [sampling_interval, sensor, target, sock])
     probe.start()
 
     timestamp, pid_cpu_usage = mesure_cpu_usage()
@@ -86,50 +85,47 @@ def sensor_mesure_send(frequency,sensor,target,sock):
     cgroup_cpu_usage = {}
     global_cpu_usage = 0
 
-    for process in pid_cpu_usage.keys():
-        global_cpu_usage += float(pid_cpu_usage[process].replace(",","."))
+    for process, _ in pid_cpu_usage:
+        global_cpu_usage += float(pid_cpu_usage[process].replace(", ", "."))
 
-
-
-    for cgroup in target :
+    for cgroup in target:
         cgroup_cpu_usage[cgroup] = 0
         cgroup_pid_file = open('/sys/fs/cgroup/perf_event/' + cgroup + '/tasks', "r")
-        cgroup_pid_raw =cgroup_pid_file.read()
+        cgroup_pid_raw = cgroup_pid_file.read()
         cgroup_pid_file.close()
         pid_list = cgroup_pid_raw.split('\n')
 
         for process in pid_list:
             if process not in pid_cpu_usage.keys():
                 continue
-            cgroup_cpu_usage[cgroup] += float(pid_cpu_usage[process].replace(",","."))
+            cgroup_cpu_usage[cgroup] += float(pid_cpu_usage[process].replace(", ", "."))
 
-    tmstp = str(timestamp)
-    tmstp[tmstp.index(' ')] = 'T' # Convert to mongo timestamp format
-    report = {'timestamp':tmstp, 'sensor':str(sensor), 'target':target, 'usage':cgroup_cpu_usage, "global_cpu_usage":global_cpu_usage}
+    tmstp_tmp = str(timestamp)
+    tmstp = tmstp_tmp[:tmstp_tmp.index(' ')] + 'T' + tmstp_tmp[tmstp_tmp.index(' ') + 1:]  # Convert to mongo timestamp format
+    report = {'timestamp': tmstp, 'sensor': str(sensor), 'target': target, 'usage': cgroup_cpu_usage, "global_cpu_usage": global_cpu_usage}
     report_json = json.dumps(report)
-    send_report(sock,report_json)
+    send_report(sock, report_json)
 
 
 def main():
     """Initialize the sensor and run it core function at the frequency required by the config"""
 
-
-    if len(sys.argv) == 0 :
-        print("Precise config file name : ")
+    if len(sys.argv) == 1:
+        print("Precise config file name: ")
         file_name = input()
     else:
         file_name = sys.argv[1]
 
-    if len(file_name) < 5 :
-        logging.error("Error : the config file must be a .json")
+    if len(file_name) < 5:
+        logging.error("Error: the config file must be a .json")
     if file_name[-5:] != '.json':
-        logging.error("Error : the config file must be a .json")
+        logging.error("Error: the config file must be a .json")
 
     config = read_config(file_name)
 
-    sensor=config['name']
-    target=config['target']
-    frequency = int(config['frequency'])
+    sensor = config['name']
+    target = config['target']
+    sampling_interval = int(config['sampling-interval'])
 
     output = config['output']
 
@@ -140,7 +136,7 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
 
-    sensor_mesure_send(frequency, sensor,target,sock)
+    sensor_mesure_send(sampling_interval, sensor, target, sock)
 
 
 main()
